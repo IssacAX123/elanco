a336247f0d5b27dda7fd9d848a37722c951bbab5
Issac Abraham <issaca32@gmail.com>
2021-11-22 16:27:18 +0000

    creates routes for each applications

--- src/App.js
+++ src/App.js
@@ -2,15 +2,25 @@ import './App.css';
 import ApplicationOverview from "./ApplicationOverview";
 import ApplicationsList from "./ApplicationsList";
 import ApplicationPage from "./ApplicationPage";
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import baseURL from "./BaseURL";
+import {useEffect, useState} from "react/cjs/react.production.min";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <ApplicationPage name={"1080p-Lock"}/>
-      </header>
-    </div>
-  );
+    let json =[]
+    fetch(`${baseURL}/applications/`).then(response =>{return response.json();}).then(data =>{json=data})
+    return (
+      <Router>
+        <div className="App">
+          <header className="App-header">
+              <Routes>
+                  <Route path="/" exact element={<ApplicationsList/>}/>
+                  {json.map(item => <Route path={"/" + item} exact element={<ApplicationsList name={item}/>}/>)}
+              </Routes>
+          </header>
+        </div>
+      </Router>
+    );
 }
 
 export default App;
