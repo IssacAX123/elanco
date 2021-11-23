2e8c842614e893b730da5b8a3fb213da912f8ded
Issac Abraham <issaca32@gmail.com>
2021-11-23 10:28:47 +0000

    creates checklist of resources to filter application

--- src/ApplicationsList.js
+++ src/ApplicationsList.js
@@ -5,13 +5,22 @@ import ApplicationOverview from "./ApplicationOverview";
 import {Link} from "react-router-dom";
 
 function ApplicationsList(props) {
+    const [resourceJson, setResourcesJson] = useState([])
+    useEffect(() =>{
+        fetch(`${baseURL}/resources/`).then(response =>{return response.json();}).then(data =>{setResourcesJson(data)})
+    }, [])
+
     const [json, setJson] = useState([])
     useEffect(() =>{
         fetch(`${baseURL}/applications/`).then(response =>{return response.json();}).then(data =>{setJson(data)})
-    }, [])
+    }, [json])
 
     return (
         <div>
+            <div id="filter">
+                {resourceJson.map((item) => <div className={"checkbox"}><input type="checkbox" id={item} name={item} value={item}/><label
+                    htmlFor={item}>{item}</label></div>)}
+            </div>
             {json.map(item => <Link style={{ textDecoration: 'none'}} to={"/"+item}><ApplicationOverview name={item}/></Link>)}
         </div>
     );
